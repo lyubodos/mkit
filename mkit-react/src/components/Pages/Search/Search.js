2cc416e4d0f3080eb7787fca2ca58276449a2a08
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-03-11 15:57:28 +0100

    Search component optimize
    Search component was optimized with the implementation of the reusable
    Input component. Users can now search from the search view page as well.
    
    Implementation is required for a better UX.

--- mkit-react/src/components/Pages/Search/Search.js
+++ mkit-react/src/components/Pages/Search/Search.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
 
 import classes from "./Search.module.css";
 
@@ -6,6 +8,14 @@ export default function Search() {
   return (
     <div className={classes.search}>
       <h1>Search</h1>
+      <div className={classes['search-engine']}>
+      <Input placeholder="Search by movie title..." />
+      <Button>Search</Button>
+      </div>
+
+      <section>
+        
+      </section>
     </div>
   );
 }
