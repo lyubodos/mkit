3372bdc9207aed41f39b56c4c9026794e58afb8e
Vasilev <Lyubomir.Vasilev@adesso.bg>
2022-03-11 13:02:09 +0100

    Home route add
    Routing for the root/home destination has been added to the App.js main
    component.
    
    Implementation was required for basic routing functionality.

--- mkit-react/src/App.js
+++ mkit-react/src/App.js
@@ -1,10 +1,14 @@
 import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './components/Layouts/Header/Header';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
    <Router>
      <Header/>
+     <Switch>
+       <Route path="/" component={Home} exact/>
+     </Switch>
    </Router>
   );
 }
